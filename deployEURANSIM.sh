054a3d865272973d3af24de95f194c2ac4a2fed6
Lapenta Francesco Davide <37077655+lapentad@users.noreply.github.com>
2023-05-23 16:07:50 +0100

    Update deployEURANSIM.sh

--- deployEURANSIM.sh
+++ deployEURANSIM.sh
@@ -4,21 +4,15 @@ for context in $(kubectl config get-contexts --no-headers --output name); do
     kubectl rollout status daemonset/kube-multus-ds -n kube-system --timeout=3m
 done
 
-
-
 helm -n free5gc install v2 towards5gs/ueransim
 
-
-
 export POD_NAME=$(kubectl get pods -n free5gc -l "component=ue" -o jsonpath="{.items[0].metadata.name}")
-
-
-
 while [[ $(kubectl get pods -n free5gc $POD_NAME -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
-sleep 5
-kubectl -n free5gc exec -it $POD_NAME -- ip address
-
-
-
-echo "\n\nexport POD_NAME=$(kubectl get pods -n free5gc -l "component=ue" -o jsonpath="{.items[0].metadata.name}")\nkubectl -n free5gc exec -it $POD_NAME -- ping -I uesimtun0 www.google.com"
-
+echo "UE pod ready"
+echo "Now you can:"
+echo "export POD_NAME=$(kubectl get pods -n free5gc -l "component=ue" -o jsonpath="{.items[0].metadata.name}")"
+echo ""
+echo "kubectl -n free5gc exec -it $POD_NAME -- ping -I uesimtun0 www.google.com"
+echo ""
+kubectl --namespace free5gc exec -it $POD_NAME -- ip address
+ 
