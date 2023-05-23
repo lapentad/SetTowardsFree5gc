87b1a048713f236f643d2d1259a2c969221b7687
Lapenta Francesco Davide <37077655+lapentad@users.noreply.github.com>
2023-05-23 17:01:21 +0100

    Update deployEURANSIM.sh

--- deployEURANSIM.sh
+++ deployEURANSIM.sh
@@ -9,3 +9,5 @@ helm -n free5gc install v2 towards5gs/ueransim
 export POD_NAME=$(kubectl get pods -n free5gc -l "component=ue" -o jsonpath="{.items[0].metadata.name}")
 while [[ $(kubectl get pods -n free5gc $POD_NAME -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" && sleep 1; done
 echo "$POD_NAME ready"
+echo "Try:"
+echo "kubectl exec -n free5gc -it \$(kubectl get pods -n free5gc -l "component=ue" -o jsonpath="{.items[0].metadata.name}") -- curl --interface uesimtun0 https://www.google.com"
