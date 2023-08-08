c953668501b6e6137be985d0c1da7d6bd3762f8b
lapentafd <francesco.lapenta@est.tech>
2023-08-08 09:33:18 +0100

    Fix typo

--- provision.sh
+++ provision.sh
@@ -121,7 +121,7 @@ installMongo() {
 		sudo systemctl status mongod
 	else
 		echo "Mongo is installed."
-	fi	
+	fi
 }
 
 installKind() {
@@ -137,7 +137,7 @@ installKind() {
 }
 
 checkNetwork() {
-	NIC=ip route | grep default | awk '{print $5}'
+        NIC=$(ip route | grep default | awk '{print $5}')
 	sudo sysctl -w net.ipv4.ip_forward=1
 	sudo iptables -t nat -A POSTROUTING -o $NIC -j MASQUERADE
 	sudo systemctl stop ufw
