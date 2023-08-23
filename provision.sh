5a23fa66511d998a367366b9236f5c15b251e6a1
Lapenta Francesco Davide <37077655+lapentad@users.noreply.github.com>
2023-08-23 12:02:41 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -28,11 +28,14 @@ checkKernel() {
 checkDocker() {
 	if ! command -v docker &> /dev/null; then
 		echo "Docker is not installed. Installing it..."
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		sudo add-apt-repository \
-		   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-		   $(lsb_release -cs) \
-		   stable"
+		sudo apt-get update -y
+		sudo mkdir -p /etc/apt/keyrings
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+		sudo chmod a+r /etc/apt/keyrings/docker.gpg
+		echo \
+		  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+		  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+		  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		sudo apt-get update -y
 		sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 		sudo usermod -aG docker $USER
