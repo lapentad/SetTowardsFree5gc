21614a2d1f83b56984db5804cc6518cd451ad7f6
Lapenta Francesco Davide <37077655+lapentad@users.noreply.github.com>
2024-09-30 10:53:45 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -152,6 +152,21 @@ checkNetwork() {
 	sudo iptables -I FORWARD 1 -j ACCEPT
 }
 
+unistallDocker() {
+        sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
+        sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
+	sudo rm -rf /var/lib/docker /etc/docker
+	sudo rm /etc/apparmor.d/docker
+	sudo groupdel docker
+	sudo rm -rf /var/run/docker.sock
+	sudo rm -rf /var/lib/containerd
+	sudo rm -r ~/.docker
+}
+
+installDockerViaScript() {
+	wget -qO- https://get.docker.com/ | sh
+}
+
 sudolessUser
 checkBasics
 checkKernel
